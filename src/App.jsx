9287ca835f07c652933eb37c8648dcee61f910ae
Sarun Daunghirun <125266560+sdsarun@users.noreply.github.com>
2023-07-07 16:37:42 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import LoadingPage from './pages/LoadingPage.jsx';
 
 function App() {
   const { VITE_HOST, VITE_API } = import.meta.env;
-  const { data, isLoading, error } = useSWR(VITE_HOST + VITE_API, fetcher);
 
+  const cacheData = JSON.parse(localStorage.getItem("data"));
+  if (cacheData) {
+    return <BarRace data={cacheData} />
+  } 
+
+  const { data, isLoading, error } = useSWR(VITE_HOST + VITE_API, fetcher);
   if (isLoading) {
     return <LoadingPage />;
   }
-
+  
   if (error) {
     return "!Oops, Something happening on backend server do something else and comeback again.";
   }
-
+  
+  localStorage.setItem("data", JSON.stringify(data));
+  
   return (
     <>
       <BarRace data={data} />
